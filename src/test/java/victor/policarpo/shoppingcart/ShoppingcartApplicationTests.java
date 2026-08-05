5f90ed1e85fd57d9274959b7a8d340488c3d318e
Victor-Policarpo <victorpolicarposilva@gmail.com>
2026-08-05 12:11:53 -0300

    initial commit

---
+++ src/test/java/victor/policarpo/shoppingcart/ShoppingcartApplicationTests.java
@@ -0,0 +1,13 @@
+package victor.policarpo.shoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingcartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
